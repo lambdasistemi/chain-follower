cd1af4595e19db1c4eb4daeeca3f277247fc7f16
Paolo Veronelli <paolo.veronelli@gmail.com>
2026-03-19 20:30:32 +0000

    chore: use haskell.nix with GHC 9.8.4

--- flake.nix
+++ flake.nix
@@ -1,29 +1,45 @@
 {
   description = "Abstract chain follower types";
+  nixConfig = {
+    extra-substituters = [ "https://cache.iog.io" ];
+    extra-trusted-public-keys =
+      [ "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" ];
+  };
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    haskellNix.url =
+      "github:input-output-hk/haskell.nix/baa6a549ce876e9c44c494a12116f178f1becbe6";
+    nixpkgs.follows = "haskellNix/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
-  outputs = inputs@{ nixpkgs, flake-parts, ... }:
+  outputs = inputs@{ self, nixpkgs, flake-parts, haskellNix, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" "aarch64-darwin" ];
-      perSystem = { pkgs, ... }:
+      perSystem = { system, ... }:
         let
-          hp = pkgs.haskellPackages;
-          chain-follower = hp.callCabal2nix "chain-follower" ./. { };
-        in {
-          packages.default = chain-follower;
-          devShells.default = hp.shellFor {
-            packages = _: [ chain-follower ];
-            nativeBuildInputs = [
-              hp.cabal-install
-              hp.fourmolu
-              hp.hlint
-              hp.cabal-fmt
-              pkgs.just
-              pkgs.nixfmt-classic
-            ];
+          pkgs = import nixpkgs {
+            overlays = [ haskellNix.overlay ];
+            inherit system;
           };
+          indexState = "2025-12-07T00:00:00Z";
+          indexTool = { index-state = indexState; };
+          project = pkgs.haskell-nix.cabalProject' {
+            name = "chain-follower";
+            src = ./.;
+            compiler-nix-name = "ghc984";
+            shell = {
+              tools = {
+                cabal = indexTool;
+                cabal-fmt = indexTool;
+                fourmolu = indexTool;
+                hlint = indexTool;
+              };
+              buildInputs = [ pkgs.just pkgs.nixfmt-classic ];
+            };
+          };
+        in {
+          packages.default =
+            project.hsPkgs.chain-follower.components.library;
+          devShells.default = project.shell;
         };
     };
 }
